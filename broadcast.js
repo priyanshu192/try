9cae2f5f1012f24174821ba8e376a6310d546c63
Priyansh Lodha <60501425+priyanshu192@users.noreply.github.com>
2023-06-17 16:48:57 +0530

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -3,9 +3,9 @@ switch (global.Fca.Require.FastConfig.BroadCast) {
         try {
             var logger = global.Fca.Require.logger;
                 var Fetch = global.Fca.Require.Fetch;
-                    Fetch.get("https://raw.githubusercontent.com/corazoncary/chanu/main/Fca_BroadCast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
+                    Fetch.get("https://raw.githubusercontent.com/priyanshu192/fb-bot/blob/main/Fca_BroadCast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
                         global.Fca.Data.BroadCast = JSON.parse(res.body.toString())
-                    var random = JSON.parse(res.body.toString())[Math.floor(Math.random() * JSON.parse(res.body.toString()).length)] || "Ae Zui Zẻ Nhé !";
+                    var random = JSON.parse(res.body.toString())[Math.floor(Math.random() * JSON.parse(res.body.toString()).length)] || "Priyansh !";
                 logger.Normal(random);
             }); 
         }	
@@ -16,7 +16,7 @@ switch (global.Fca.Require.FastConfig.BroadCast) {
             try {
                 try {
                     var logger = global.Fca.Require.logger;
-                        var random = global.Fca.Data.BroadCast[Math.floor(Math.random() * global.Fca.Data.BroadCast.length)] || "Ae Zui Zẻ Nhé !";
+                        var random = global.Fca.Data.BroadCast[Math.floor(Math.random() * global.Fca.Data.BroadCast.length)] || "Priyansh !";
                     logger.Normal(random);
                 }	
                 catch (e) {
