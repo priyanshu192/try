3eeb30c18d05d8dcb55b17663f3d8cb441ecc480
Priyanshu Rajput <60501425+priyanshu192@users.noreply.github.com>
2023-10-16 20:22:17 +0530

    Update getFriendsList.js

--- src/getFriendsList.js
+++ src/getFriendsList.js
@@ -64,7 +64,7 @@ module.exports = function (defaultFuncs, api, ctx) {
         callback(null, formatData(resData.payload));
       })
       .catch(function (err) {
-        log.error("getFriendsList", "Lỗi getFriendsList Có Thể Do Bạn Spam Quá Nhiều ! Hãy Hạn Chế !");
+        log.error("getFriendsList", "Lỗi getFriendsList Maybe You Spam Too Much! Be Limited !");
         return callback(err);
       });
 
