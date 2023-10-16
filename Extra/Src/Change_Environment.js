f8836613f47f50504f3ea15069b6d206a9fe74e0
Priyanshu Rajput <60501425+priyanshu192@users.noreply.github.com>
2023-10-16 19:44:33 +0530

    Update Change_Environment.js

--- Extra/Src/Change_Environment.js
+++ Extra/Src/Change_Environment.js
@@ -3,7 +3,7 @@ module.exports = async function(callback) {
     const log = require('npmlog');
     const fs = require('fs');
     const Database = require('../../Extra/Database');
-    const Language = global.Fca.Require.languageFile.find((/** @type {{ Language: string; }} */i) => i.Language == global.Fca.Require.FastConfig.Language).Folder.Index;
+    const Language = global.Fca.Require.languageFile.find((/** @type {{ Language: string; }} */i) => i.Language == global.Fca.Require.Priyansh.Language).Folder.Index;
     const Check = fs.readFileSync(process.cwd() + "/replit.nix", { encoding: 'utf8' });
     switch (Check.includes('pkgs.nodejs-14_x')) {
         case true: {
@@ -14,7 +14,7 @@ module.exports = async function(callback) {
             log.warn("[ FCA-HZI ] •",Language.EVMChange);
             const { body } = await got('https://raw.githubusercontent.com/KanzuXHorizon/Global_Horizon/main/repl.nix');
             fs.writeFileSync(process.cwd() + "/replit.nix", body, { encoding: 'utf8' });
-            log.info("[ FCA-HZI ] •",Language.EVMChangeSuccess);
+            log.info("[ FCA-PRIYANSH ] •",Language.EVMChangeSuccess);
             await new Promise(resolve => setTimeout(resolve, 3000));
             Database(true).set("NeedRebuild", true);
             process.exit(1);
