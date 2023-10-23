72c5773b7e8dad1b1b4e88aac4a7bde4123aeb71
Priyanshu Rajput <60501425+priyanshu192@users.noreply.github.com>
2023-10-23 21:14:33 +0530

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -3,7 +3,7 @@ switch (global.Fca.Require.Priyansh.BroadCast) {
         try {
             var logger = global.Fca.Require.logger;
                 var Fetch = global.Fca.Require.Fetch;
-                    Fetch.get("https://raw.githubusercontent.com/priyanshu192/fb-bot/blob/main/Fca_BroadCast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
+                    Fetch.get("https://raw.githubusercontent.com/priyanshu192/fb-bot/main/Fca_BroadCast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
                         global.Fca.Data.BroadCast = JSON.parse(res.body.toString())
                     var random = JSON.parse(res.body.toString())[Math.floor(Math.random() * JSON.parse(res.body.toString()).length)] || "Priyansh !";
                 logger.Normal(random);
